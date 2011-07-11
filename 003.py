3c4afc33a4e2f024c05bbebd55e9d3a4c4fbaad3
César Frias <cagfrias@gmail.com>
2011-07-11 14:27:40 -0300

    Assim fica mais legivel

--- 003.py
+++ 003.py
@@ -1,5 +1,6 @@
+from math import sqrt
 numero = 600851475143
-for i in xrange(1, int(numero**0.5)):
+for i in xrange(1, int(sqrt(numero))):
     if numero % i == 0:
         maior_divisor = i
         numero = numero / i
